4b39cfc13463b401050d2d984eb3480be1c04557
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-09-18 00:21:25 -0500

    fixing file name

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -40,6 +40,7 @@ export class PipelineStack extends cdk.Stack {
           environment: {
             buildImage: LinuxBuildImage.STANDARD_5_0
           },
+          
           buildSpec: BuildSpec.fromSourceFilename('buil-specs/cdk-build-spec.yml')
         })
       })]
