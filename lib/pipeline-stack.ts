9ae07a298ddf8071ddd6a0dcbea95db3ead6b930
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-09-18 22:16:30 -0500

    Pipeline Update Stage

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -2,7 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { SecretValue } from 'aws-cdk-lib';
 import { Artifact, Pipeline } from 'aws-cdk-lib/aws-codepipeline';
 import { Construct } from 'constructs';
-import { CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
+import { CloudFormationCreateUpdateStackAction, CodeBuildAction, GitHubSourceAction } from 'aws-cdk-lib/aws-codepipeline-actions';
 import { PipelineProject, LinuxBuildImage, BuildSpec } from 'aws-cdk-lib/aws-codebuild';
 
 export class PipelineStack extends cdk.Stack {
@@ -45,5 +45,17 @@ export class PipelineStack extends cdk.Stack {
         })
       })]
     });
+
+    pipeline.addStage({
+      stageName: 'Pipeline_Update',
+      actions: [
+        new CloudFormationCreateUpdateStackAction({
+          actionName: 'Pipeline_Update',
+          stackName: 'PipelineStack',
+          templatePath: cdkBuildOutput.atPath('PipelineStack.template.json'),
+          adminPermissions: true
+        })
+      ]
+    })
   }
 }
