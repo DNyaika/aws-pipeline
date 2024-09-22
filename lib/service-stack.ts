8354f9ee4ea5f9d7011c6017b5af2205cda466b1
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-09-22 10:00:18 -0500

    Add production stage and deploy service stack

---
+++ lib/service-stack.ts
@@ -0,0 +1,26 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Code, Function, Runtime, InlineCode, CfnParametersCode } from 'aws-cdk-lib/aws-lambda';
+import { HttpApi } from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+
+export class ServiceStack extends Stack{
+    public readonly serviceCode: CfnParametersCode;
+    constructor(scope: Construct, id: string, props?: StackProps){
+        super(scope, id, props);
+        this.serviceCode = Code.fromCfnParameters();
+
+        const lambda = new Function(this, 'ServiceLambda', {
+            runtime: Runtime.NODEJS_18_X,
+            handler: 'src/lambda.handler',
+            code: this.serviceCode,
+            functionName: 'ServiceLambda'
+        });
+
+        new HttpApi(this, 'ServiceApi', {
+            defaultIntegration: new HttpLambdaIntegration('ServiceLambdaIntegration', lambda),
+            apiName: 'ServiceApi'
+        });
+
+    }
+}
