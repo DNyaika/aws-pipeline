9c1b5d1448e6d031581d548ed0a6e9e854d90fd2
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-10-01 23:51:11 -0500

    Add gradual deployments to service stack in prod

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,9 +1,11 @@
-import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-import { Alias, Function, Runtime, Code, CfnParametersCode } from 'aws-cdk-lib/aws-lambda';
-import { LambdaDeploymentGroup, LambdaDeploymentConfig } from 'aws-cdk-lib/aws-codedeploy';
 import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Code, Function, Runtime, InlineCode, CfnParametersCode } from 'aws-cdk-lib/aws-lambda';
 import { HttpApi } from 'aws-cdk-lib/aws-apigatewayv2';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { Alias } from 'aws-cdk-lib/aws-lambda';
+import { Version } from 'aws-cdk-lib/aws-lambda';
+import { LambdaDeploymentGroup, LambdaDeploymentConfig } from 'aws-cdk-lib/aws-codedeploy';
 
 interface ServiceStackProps extends StackProps {
     stageName: string;
@@ -12,7 +14,6 @@ interface ServiceStackProps extends StackProps {
 export class ServiceStack extends Stack {
     public readonly serviceCode: CfnParametersCode;
     public readonly serviceEndpointOutput: CfnOutput;
-
     constructor(scope: Construct, id: string, props?: ServiceStackProps) {
         super(scope, id, props);
         this.serviceCode = Code.fromCfnParameters();
@@ -22,7 +23,7 @@ export class ServiceStack extends Stack {
             handler: 'src/lambda.handler',
             code: this.serviceCode,
             functionName: `ServiceLambda${props?.stageName}`,
-            description: `Generated on ${new Date().toISOString()}`,
+            description: 'Generated on ${new Date().toISOString()}',
         });
 
         // Sanitize stageName to remove invalid characters and ensure length constraints
@@ -30,23 +31,28 @@ export class ServiceStack extends Stack {
 
         const alias = new Alias(this, 'ServiceLambdaAlias', {
             version: lambda.currentVersion,
-            aliasName: `ServiceLambdaAlias${sanitizedStageName}`,
+            aliasName: 'ServiceLambdaAlias${sanitizedStageName}',
+
         });
 
         const httpApi = new HttpApi(this, 'ServiceApi', {
             defaultIntegration: new HttpLambdaIntegration('ServiceLambdaIntegration', alias),
-            apiName: `ServiceApi${sanitizedStageName}`,
+            apiName: `ServiceApi${props?.stageName}`,
         });
 
         if (props?.stageName === 'Prod') {
-            new LambdaDeploymentGroup(this, 'ServiceDeploymentGroup', {
+            new LambdaDeploymentGroup(this, 'DeploymentGroup', {
                 alias: alias,
                 deploymentConfig: LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
             });
         }
 
+
+
         this.serviceEndpointOutput = new CfnOutput(this, 'ApiEndpointOutput', {
-            value: httpApi.url!,
-        });
+            exportName: `ServiceEndpoint${props?.stageName}`,
+            value: httpApi.apiEndpoint,
+            description: 'API Endpoint'
+        })
     }
 }
