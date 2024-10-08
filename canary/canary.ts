303cd3954cd0aa15c43225c65d73cc3553d50a27
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-10-07 23:48:44 -0500

    Create Canary for service endpoint

--- canary/canary.ts
+++ canary/canary.ts
@@ -5,8 +5,7 @@ const synthetics = require('@aws-cdk/aws-synthetics');
 const canary = async function () {  
     await synthetics.executeHttpStep(
         "Verify API returns successful response",
-        process.env.API_ENDPOINT
-        
+        process.env.API_ENDPOINT,
     );
 }
 
