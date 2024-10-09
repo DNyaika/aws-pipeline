9dc5d5f14fec93a7e0c7a5803b7e582dab2b2357
david nyaika <davidnyaika@MacBook-Pro-2.lan>
2024-10-08 23:34:08 -0500

    Create Canary for service endpoint

--- lib/service-stack.ts
+++ lib/service-stack.ts
@@ -1,13 +1,16 @@
-import * as path from 'path';
-import * as fs from 'fs';
-import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
-import { Alias, Function, Runtime, Code, CfnParametersCode } from 'aws-cdk-lib/aws-lambda';
-import { LambdaDeploymentGroup, LambdaDeploymentConfig } from 'aws-cdk-lib/aws-codedeploy';
-import { Stack, StackProps, CfnOutput, Duration } from 'aws-cdk-lib';
+import { Stack, StackProps, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
+import { Code, Function, Runtime, InlineCode, CfnParametersCode } from 'aws-cdk-lib/aws-lambda';
 import { HttpApi } from 'aws-cdk-lib/aws-apigatewayv2';
-import { TreatMissingData, Statistic, ComparisonOperator } from 'aws-cdk-lib/aws-cloudwatch';
-import { Canary, Code as SyntheticsCode, Runtime as SyntheticsRuntime, Schedule } from 'aws-cdk-lib/aws-synthetics';
+import { HttpLambdaIntegration } from 'aws-cdk-lib/aws-apigatewayv2-integrations';
+import { Alias } from 'aws-cdk-lib/aws-lambda';
+import { Version } from 'aws-cdk-lib/aws-lambda';
+import { LambdaDeploymentGroup, LambdaDeploymentConfig } from 'aws-cdk-lib/aws-codedeploy';
+import { ComparisonOperator, Statistic, TreatMissingData } from 'aws-cdk-lib/aws-cloudwatch';
+import { Duration } from 'aws-cdk-lib';
+import { Effect, PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import { Service } from 'aws-cdk-lib/aws-servicediscovery';
+import { ServiceHealthCanary } from './constructs/service-health-canary';
 
 interface ServiceStackProps extends StackProps {
     stageName: string;
@@ -16,7 +19,6 @@ interface ServiceStackProps extends StackProps {
 export class ServiceStack extends Stack {
     public readonly serviceCode: CfnParametersCode;
     public readonly serviceEndpointOutput: CfnOutput;
-
     constructor(scope: Construct, id: string, props?: ServiceStackProps) {
         super(scope, id, props);
         this.serviceCode = Code.fromCfnParameters();
@@ -29,12 +31,21 @@ export class ServiceStack extends Stack {
             description: `Generated on ${new Date().toISOString()}`,
         });
 
-        // Sanitize stageName to remove invalid characters and ensure length constraints
-        const sanitizedStageName = props?.stageName.replace(/[^a-zA-Z0-9]/g, '').substring(0, 20);
+        // Grant permissions for CloudWatch Logs
+        lambda.addToRolePolicy(new PolicyStatement({
+            effect: Effect.ALLOW,
+            actions: [
+                'logs:CreateLogGroup',
+                'logs:CreateLogStream',
+                'logs:PutLogEvents',
+            ],
+            resources: ['*'], // You can restrict this to specific log groups if needed
+        }));
 
         const alias = new Alias(this, 'ServiceLambdaAlias', {
             version: lambda.currentVersion,
-            aliasName: `ServiceLambdaAlias${sanitizedStageName}`,
+            aliasName: `ServiceLambdaAlias${props?.stageName}`,
+
         });
 
         const httpApi = new HttpApi(this, 'ServiceApi', {
@@ -43,47 +54,41 @@ export class ServiceStack extends Stack {
         });
 
         if (props?.stageName === 'Prod') {
-            new LambdaDeploymentGroup(this, 'DeploymentGroup', {
+            console.log('Entering the if statement: stageName is Prod');
+            new LambdaDeploymentGroup(this, 'ServiceDeploymentGroup', {
                 alias: alias,
                 deploymentConfig: LambdaDeploymentConfig.CANARY_10PERCENT_5MINUTES,
+                autoRollback: {
+                    deploymentInAlarm: true,
+                },
+                alarms: [
+                    httpApi.metricServerError()
+                        .with({
+                            period: Duration.minutes(1),
+                            statistic: Statistic.SUM,
+                        })
+                        .createAlarm(this, 'ServiceErrorAlarm', {
+                            threshold: 1,
+                            alarmDescription: 'Service is experiencing errors',
+                            alarmName: `ServiceErrorAlarm${props?.stageName}`,
+                            evaluationPeriods: 1,
+                            treatMissingData: TreatMissingData.NOT_BREACHING,
+                            comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
+                        }),
+                ],
+
+            });
+            new ServiceHealthCanary(this, 'ServiceHealthCanary', {
+                apiEndpoint: httpApi.apiEndpoint,
+                canaryName: "new2-service-canary",
             });
         }
 
-        httpApi.metricServerError()
-            .with({
-                period: Duration.minutes(1),
-                statistic: Statistic.SUM,
-            })
-            .createAlarm(this, 'ServiceErrorAlarm', {
-                threshold: 1,
-                alarmDescription: 'Service is experiencing errors',
-                alarmName: `ServiceErrorAlarm${props?.stageName}`,
-                evaluationPeriods: 1,
-                treatMissingData: TreatMissingData.NOT_BREACHING,
-                comparisonOperator: ComparisonOperator.GREATER_THAN_OR_EQUAL_TO_THRESHOLD,
-            });
-
-        const canaryScriptPath = path.join(__dirname, '../../canary.ts');
-        const canaryScript = fs.readFileSync(canaryScriptPath, { encoding: 'utf-8' });
-
-        new Canary(this, 'ServiceHealthCanary', {
-            runtime: SyntheticsRuntime.SYNTHETICS_NODEJS_PUPPETEER_3_8,
-            canaryName: 'service-canary',
-            schedule: Schedule.rate(Duration.minutes(5)),
-            environmentVariables: {
-                API_ENDPOINT: httpApi.apiEndpoint,
-            },
-            test: {
-                code: SyntheticsCode.fromInline(canaryScript),
-                handler: 'index.handler',
-            },
-            timeToLive: Duration.days(1),
-        });
 
         this.serviceEndpointOutput = new CfnOutput(this, 'ApiEndpointOutput', {
             exportName: `ServiceEndpoint${props?.stageName}`,
             value: httpApi.apiEndpoint,
             description: 'API Endpoint'
-        });
+        })
     }
 }
